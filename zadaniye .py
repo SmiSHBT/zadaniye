fb0e84ed90f60247167bebadbd12dbd7440a274f
SmiSHBT <btursunov949@gmail.com>
2024-08-12 21:12:15 +0300

    ss

---
+++ zadaniye .py
@@ -0,0 +1,48 @@
+# 1
+# chislo = int(input("vvedite chislo:  "))
+# if chislo % 2 == 0:
+#     print("chislo chotnoye")
+# elif chislo == 0 or chislo < 0:
+#     print("vvedite polojitelniye chisla")
+# else:
+#     print("chislo ne chetnoye")
+
+# 2
+# def a():
+#     g = []
+#     b = int(input("skolko vi hotite vvesti"))
+    
+#     for chislo in range(b):
+#         j = (input(f"vedite chislo {chislo + 1}: "))
+#         g.append(j)
+#         print(f"samiy malenkiy chislo :{min(g)}")
+# a()
+
+#3
+# a = str(input("vvedite slovo: "))
+# if a == a[::-1]:
+#     print("palindrom")
+# else:
+#     print("ne palindrom")
+#4
+# def sum():
+#     count = int(input("Сколько чисел вы хотите ввести? "))
+    
+#     total_sum = 0
+
+#     for i in range(count):
+#         number = int(input(f"vvedite chislo {i + 1}: "))
+#         total_sum += number
+
+#     print(f"sum: {total_sum}")
+
+# sum()
+
+#5
+# chisl = int(input("vedite chislo do kotoroy vi hotite vivesti: "))
+# for i in range(1, chisl):
+#      for j in range(1, 11):
+#          print(f"{i} x {j} = {i * j}")
+#      print()    
+
+#6
